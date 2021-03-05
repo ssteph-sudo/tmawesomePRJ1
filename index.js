300d69efc9f0c77e1a7ca78958b99cdea19b03ba
MrPork1 <kelly.sean98@gmail.com>
2021-03-04 16:37:21 -0800

    Added delete button

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let plantArray = [];
 
 
 
-let PlantObject = function (ID,pPlantName, pGerminated, pPlanted, pBloomed, pQuantity, pScoScale,pFoodDate,pNotes) {
-    this.ID = ID;
+let PlantObject = function (pPlantName, pGerminated, pPlanted, pBloomed, pQuantity, pScoScale,pFoodDate,pNotes) {
+    this.ID = Math.random().toString(16).slice(5);
     this.PlantName = pPlantName;
     this.Germinated = pGerminated;
     this.Planted = pPlanted;
@@ -15,8 +15,8 @@ let PlantObject = function (ID,pPlantName, pGerminated, pPlanted, pBloomed, pQua
     this.Notes= pNotes;
 }
 
-plantArray.push(new PlantObject(1,"Sunflower", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Needs lots of water."));
-plantArray.push(new PlantObject(2,"Peas", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Grows well in partial shade."));
+plantArray.push(new PlantObject("Sunflower", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Needs lots of water."));
+plantArray.push(new PlantObject("Peas", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Grows well in partial shade."));
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -27,22 +27,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     $(document).on("pagebeforeshow", "#Edit", function (event) {   
         let localID = document.getElementById("IDparmHere").innerHTML;
-        document.getElementById("PlantName").innerHTML = "Name of Plant: " + plantArray[localID - 1].PlantName;
-        document.getElementById("Germinated").innerHTML = "Date Germiinated: " + plantArray[localID - 1].Germinated;
-        document.getElementById("Bloomed").innerHTML = "Time to Bloom: " + plantArray[localID - 1].Bloomed;
-        document.getElementById("FoodDate").innerHTML = "Date Last Given Food: " + plantArray[localID - 1].FoodDate;
-        document.getElementById("Quantity").innerHTML = "Quantity: " + plantArray[localID - 1].Quantity;
-        document.getElementById("Scoville").innerHTML = "Scoville Scale: " + plantArray[localID - 1].ScovilleScale;
-        document.getElementById("Notes").innerHTML = "Notes: " + plantArray[localID - 1].Notes;
+        let arrayPointer = GetArrayPointer(localID);
+        document.getElementById("PlantName").innerHTML = "Name of Plant: " + plantArray[arrayPointer].PlantName;
+        document.getElementById("Germinated").innerHTML = "Date Germiinated: " + plantArray[arrayPointer].Germinated;
+        document.getElementById("Bloomed").innerHTML = "Time to Bloom: " + plantArray[arrayPointer].Bloomed;
+        document.getElementById("FoodDate").innerHTML = "Date Last Given Food: " + plantArray[arrayPointer].FoodDate;
+        document.getElementById("Quantity").innerHTML = "Quantity: " + plantArray[arrayPointer].Quantity;
+        document.getElementById("Scoville").innerHTML = "Scoville Scale: " + plantArray[arrayPointer].ScovilleScale;
+        document.getElementById("Notes").innerHTML = "Notes: " + plantArray[arrayPointer].Notes;
     });
- 
+
+    document.getElementById("submitPlantButton").addEventListener("click", function () {
+        document.location.href = "index.html#ListAll";
+    });
+
+    document.getElementById("deletePlantButton").addEventListener("click", function () {
+        let localID = document.getElementById("IDparmHere").innerHTML;
+        deleteMovie(localID);
+        showPlantList();
+        document.location.href = "index.html#ListAll";
+    });
 
 });  
 
 
 
 function SubmitPlant() {
-    location.href="#ListAll";
+    document.location.href="index.html#ListAll";
 }
 
 function showPlantList() {
@@ -56,16 +67,16 @@ function showPlantList() {
         divPlantList.removeChild(divPlantList.firstChild);
     };
 
-    var ul = document.createElement('ul');
+    var ol = document.createElement('ol');
 
     plantArray.forEach(function (element,) {   
         var li = document.createElement('li');
         li.classList.add('onePlant'); 
         li.setAttribute("data-parm", element.ID);
-        li.innerHTML = element.ID + ":  " + element.PlantName + "   " + element.Germinated;
-        ul.appendChild(li);
+        li.innerHTML = element.PlantName + "   " + element.Germinated;
+        ol.appendChild(li);
     });
-    myUI.appendChild(ul);
+    myUI.appendChild(ol);
 
 
     var myArray = document.getElementsByClassName("onePlant");
@@ -75,8 +86,20 @@ function showPlantList() {
         var parm = this.getAttribute("data-parm");  
  
         document.getElementById("IDparmHere").innerHTML = parm;
-    
         document.location.href = "index.html#Edit";
         });
     });
     }
+
+function deleteMovie(movieID) {
+    let arrayPointer = GetArrayPointer(movieID);
+    plantArray.splice(arrayPointer, 1);
+}
+
+function GetArrayPointer(localID) {
+    for (let i = 0; i < plantArray.length; i++) {
+        if (localID == plantArray[i].ID) {
+            return i;
+        }
+    }
+}
