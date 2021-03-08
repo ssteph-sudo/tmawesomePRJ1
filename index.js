202f7e0581e7190a8b66534f6a9d936643cd6c1d
MrPork1 <kelly.sean98@gmail.com>
2021-03-08 11:04:46 -0800

    Changed files

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ let PlantObject = function (pPlantName, pGerminated, pPlanted, pBloomed, pQuanti
     this.Notes= pNotes;
 }
 
-plantArray.push(new PlantObject("Sunflower", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Needs lots of water."));
-plantArray.push(new PlantObject("Peas", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Grows well in partial shade."));
+//plantArray.push(new PlantObject("Sunflower", "02/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Needs lots of water."));
+//plantArray.push(new PlantObject("Peas", "03/27/2021", "02/24/2021", "6-8 Weeks", "3", "40000","02/28/2021", "Grows well in partial shade."));
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -27,9 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     $(document).on("pagebeforeshow", "#Edit", function (event) {   
         let localID = document.getElementById("IDparmHere").innerHTML;
-        let arrayPointer = GetArrayPointer(localID);
+        let arrayPointer = GetLocalID(localID);
         document.getElementById("PlantName").innerHTML = "Name of Plant: " + plantArray[arrayPointer].PlantName;
-        document.getElementById("Germinated").innerHTML = "Date Germiinated: " + plantArray[arrayPointer].Germinated;
+        document.getElementById("Germinated").innerHTML = "Date Germinated: " + plantArray[arrayPointer].Germinated;
         document.getElementById("Bloomed").innerHTML = "Time to Bloom: " + plantArray[arrayPointer].Bloomed;
         document.getElementById("FoodDate").innerHTML = "Date Last Given Food: " + plantArray[arrayPointer].FoodDate;
         document.getElementById("Quantity").innerHTML = "Quantity: " + plantArray[arrayPointer].Quantity;
@@ -38,28 +38,56 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.getElementById("submitPlantButton").addEventListener("click", function () {
+
+        plantArray.push(new PlantObject(document.getElementById("plantName").value, document.getElementById("dateGerminated").value, document.getElementById("datePlanted").value,
+        document.getElementById("dateBloomed").value, document.getElementById("quantityInput").value, document.getElementById("slider1").value, document.getElementById("dateGivenFood").value,
+        document.getElementById("notesInput").value));
+
         document.location.href = "index.html#ListAll";
+        clearInputFields();
+
     });
 
     document.getElementById("deletePlantButton").addEventListener("click", function () {
         let localID = document.getElementById("IDparmHere").innerHTML;
-        deleteMovie(localID);
+        deletePlant(localID);
         showPlantList();
         document.location.href = "index.html#ListAll";
     });
 
+    document.getElementById("modifyPlantButton").addEventListener("click", function() {
+        let localID = document.getElementById("IDparmHere").innerHTML;
+        changePlantInputFields(localID);
+        document.location.href = "index.html#Modify";
+    });
+
+    document.getElementById("submitModifyButton").addEventListener("click", function() {
+        let localID = document.getElementById("IDparmHere").innerHTML;
+
+        let arrayPointer = GetLocalID(localID);
+
+        plantArray[arrayPointer] = new PlantObject(document.getElementById("modifyPlantName").value, document.getElementById("modifyDateGerminated").value,
+        document.getElementById("modifyDatePlanted").value, document.getElementById("modifyDateBloomed").value, document.getElementById("modifyQuantityInput").value,
+        document.getElementById("modifySlider1").value, document.getElementById("modifyDateGivenFood").value, document.getElementById("modifyNotesInput").value);
+
+        document.location.href = "index.html#ListAll";
+    });
+
+    document.getElementById("clearDataButton").addEventListener("click", function() {
+        clearInputFields();
+    });
+
+    document.getElementById("sortAlphabetically").addEventListener("click", function() {
+        sortByName();
+    });
+
+    document.getElementById("sortDate").addEventListener("click", function() {
+        sortByGerminatedDate();
+    });
 });  
-
-
-
-function SubmitPlant() {
-    document.location.href="index.html#ListAll";
-}
 
 function showPlantList() {
     
-
-
     let myUI = document.getElementById("divPlantList");
 
 
@@ -73,10 +101,11 @@ function showPlantList() {
         var li = document.createElement('li');
         li.classList.add('onePlant'); 
         li.setAttribute("data-parm", element.ID);
-        li.innerHTML = element.PlantName + "   " + element.Germinated;
+        li.innerHTML = element.ID + "   " + element.PlantName + "   " + element.Germinated
         ol.appendChild(li);
     });
     myUI.appendChild(ol);
+    //console.log(ol);
 
 
     var myArray = document.getElementsByClassName("onePlant");
@@ -91,15 +120,53 @@ function showPlantList() {
     });
     }
 
-function deleteMovie(movieID) {
-    let arrayPointer = GetArrayPointer(movieID);
+function deletePlant(plantID) {
+    let arrayPointer = GetLocalID(plantID);
     plantArray.splice(arrayPointer, 1);
 }
 
-function GetArrayPointer(localID) {
+function GetLocalID(localID) {
     for (let i = 0; i < plantArray.length; i++) {
         if (localID == plantArray[i].ID) {
             return i;
         }
     }
+}
+
+function clearInputFields() {
+    document.getElementById("plantName").value = "";
+    document.getElementById("dateGerminated").value = "";
+    document.getElementById("datePlanted").value = "";
+    document.getElementById("dateBloomed").value = "";
+    document.getElementById("quantityInput").value = 1;
+    document.getElementById("slider1").value = 1;
+    document.getElementById("dateGivenFood").value = "";
+    document.getElementById("notesInput").value = "";
+}
+
+function changePlantInputFields(plantID) {
+    console.log("modifying functuon running" + plantID);
+    let arrayPointer = GetLocalID(plantID);
+    let plantPointer = plantArray[arrayPointer];
+    document.getElementById("modifyHeader").innerHTML = "Modifying Current Plant: " + plantPointer.PlantName;
+    document.getElementById("modifyPlantName").value = plantPointer.PlantName;
+    document.getElementById("modifyDateGerminated").value = plantPointer.Germinated;
+    document.getElementById("modifyDatePlanted").value = plantPointer.Planted;
+    document.getElementById("modifyDateBloomed").value = plantPointer.Bloomed;
+    document.getElementById("modifyDateGivenFood").value = plantPointer.FoodDate;
+    document.getElementById("modifyQuantityInput").value = plantPointer.Quantity;
+    document.getElementById("modifySlider1").value = plantPointer.ScovilleScale;
+    document.getElementById("modifyNotesInput").value = plantPointer.Notes;
+
+    
+}
+
+function sortByName() {
+    plantArray.sort((a, b) => (a.PlantName > b.PlantName) ? 1 : -1)
+    showPlantList();
+}
+
+function sortByGerminatedDate() {
+    plantArray.sort((a, b) => (a.Germinated > b.Germinated) ? 1 : -1)
+    showPlantList();
 }
